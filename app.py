65e0ba030a10271b8e28d6080cec621b72abb88c
bosstb <tangbu@foxmail.com>
2017-11-17 12:48:34 +0800

    1

--- app.py
+++ app.py
@@ -26,6 +26,8 @@ app.register_blueprint(todos_view, url_prefix='/todos')
 class token_record(leancloud.Object):
     pass
 
+class Logdata(leancloud.Object):
+    pass
 
 class Token_CR(leancloud.Object):
     pass
@@ -91,11 +93,51 @@ def index():
             return "Missing package!"
 
 
+def getCountry(ip):
+    r = requests.post(
+        'http://api.db-ip.com/v2/c6f4413393e0ce3d120471ad41f7d7ad5bf77df0/' + str(ip))
+    country = json.loads(r.text)
+    if r.text.find('error') == -1:
+        if country["countryCode"] != 'ZZ':
+            countryName = country["countryName"]
+        else:
+            countryName = 'Unkown'
+    else:
+        countryName = 'Unkown'
+    return  countryName
+
+
+def logdata(apkg_country_result):
+    if apkg_country_result:
+        query = leancloud.Query(Logdata)
+        query.equal_to('apkg_country_result', apkg_country_result)
+        query_list = query.find()
+        tokenRecord = None
+        if query_list:
+            objId = query_list[0].get("objectId")
+            Todo = leancloud.Object.extend('Logdata')
+            logdata = Todo.create_without_data(objId)
+            logdata.set('count', query_list[0].get("count")+1)
+            logdata.save()
+        else:
+            logdata = Logdata()
+            logdata.set('apkg_country_result', apkg_country_result)
+            logdata.set('count', 1)
+            logdata.save()
+
 @app.route('/gettk', methods=["GET", "POST"])
 def time():
     if request.method == "POST":
-        print request.content_type
-        print request.form
+        ip = None
+        try:
+            ip = request.headers['x-real-ip']
+        except:
+            print 1
+        # 取国家
+        if ip:
+            countryName = getCountry(ip)
+        else:
+            countryName = "Unkown"
 
         if request.content_type == "application/json":
             args = json.loads(request.get_data())
@@ -108,59 +150,68 @@ def time():
         query = leancloud.Query(token_record)
         query.equal_to('package', apkg)
         query_list_apkg = query.find()
-        if query_list_apkg[0].get("devkey"):
-            query = leancloud.Query(Token_CR)
-            query.equal_to('package', ppkg)
-            query_list = query.find()
-            if query_list:
-                per = query_list[0].get("CR")
-                ran = random.randint(1, 100)
-                ran = float(ran)
-                if ran <= per:
-
-                    data = {
-                        "devkey": query_list_apkg[0].get("devkey"),
-                        "platform": query_list_apkg[0].get("platform"),
-                        "versionname": query_list_apkg[0].get("versionname"),
-                        "event": query_list_apkg[0].get("event"),
-                        "data": query_list_apkg[0].get("data"),
-                        "adver": query_list_apkg[0].get("adver"),
-                        "versioncode": query_list_apkg[0].get("versioncode"),
-                        "afver": query_list_apkg[0].get("afver"),
-                        "flykey": "",
-                        "msg": "",
-                        "pkg": apkg,
-                        "code": 200
-                    }
-                    return json.dumps(data)
+        if query_list_apkg:
+            if query_list_apkg[0].get("devkey"):
+                query = leancloud.Query(Token_CR)
+                query.equal_to('package', ppkg)
+                query_list = query.find()
+                if query_list:
+                    per = query_list[0].get("CR")
+                    ran = random.randint(1, 100)
+                    ran = float(ran)
+                    if ran <= per:
+                        data = {
+                            "devkey": query_list_apkg[0].get("devkey"),
+                            "platform": query_list_apkg[0].get("platform"),
+                            "versionname": query_list_apkg[0].get("versionname"),
+                            "event": query_list_apkg[0].get("event"),
+                            "data": query_list_apkg[0].get("data"),
+                            "adver": query_list_apkg[0].get("adver"),
+                            "versioncode": query_list_apkg[0].get("versioncode"),
+                            "afver": query_list_apkg[0].get("afver"),
+                            "flykey": "",
+                            "msg": "",
+                            "pkg": apkg,
+                            "code": 200
+                        }
+                        logdata(apkg + "_" + countryName + "_" + "ok")
+                        return json.dumps(data)
+                    else:
+                        data = {"data": "", "code": 400, "msg": "Prob"}
+                        logdata(apkg + "_" + countryName + "_" + "Prob")
+                        return json.dumps(data)
                 else:
-                    data = {"data": "", "code": 400, "msg": "Prob"}
-                    return json.dumps(data)
+                    per = 30
+                    ran = random.randint(1, 100)
+                    ran = float(ran)
+                    if ran <= per:
+                        data = {
+                            "devkey": query_list_apkg[0].get("devkey"),
+                            "platform": query_list_apkg[0].get("platform"),
+                            "versionname": query_list_apkg[0].get("versionname"),
+                            "event": query_list_apkg[0].get("event"),
+                            "data": query_list_apkg[0].get("data"),
+                            "adver": query_list_apkg[0].get("adver"),
+                            "versioncode": query_list_apkg[0].get("versioncode"),
+                            "afver": query_list_apkg[0].get("afver"),
+                            "flykey": "",
+                            "msg": "",
+                            "pkg": apkg,
+                            "code": 200
+                        }
+                        logdata(apkg + "_" + countryName + "_" + "ok")
+                        return json.dumps(data)
+                    else:
+                        logdata(apkg + "_" + countryName + "_" + "Prob")
+                        data = {"data": "", "code": 400, "msg": "Prob"}
+                        return json.dumps(data)
             else:
-                per = 30
-                ran = random.randint(1, 100)
-                ran = float(ran)
-                if ran <= per:
-                    data = {
-                        "devkey": query_list_apkg[0].get("devkey"),
-                        "platform": query_list_apkg[0].get("platform"),
-                        "versionname": query_list_apkg[0].get("versionname"),
-                        "event": query_list_apkg[0].get("event"),
-                        "data": query_list_apkg[0].get("data"),
-                        "adver": query_list_apkg[0].get("adver"),
-                        "versioncode": query_list_apkg[0].get("versioncode"),
-                        "afver": query_list_apkg[0].get("afver"),
-                        "flykey": "",
-                        "msg": "",
-                        "pkg": apkg,
-                        "code": 200
-                    }
-                    return json.dumps(data)
-                else:
-                    data = {"data": "", "code": 400, "msg": "Prob"}
-                    return json.dumps(data)
+                data = {"data":"", "code": 400, "msg": "No token"}
+                logdata(apkg + "_" + countryName + "_" + "No token")
+                return json.dumps(data)
         else:
-            data = {"data":"", "code": 400, "msg": "No token"}
+            data = {"data": "", "code": 400, "msg": "No token"}
+            logdata(apkg + "_" + countryName + "_" + "No pkg")
             return json.dumps(data)
     else:
         return "fuck you!"
